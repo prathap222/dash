26167da4756f0094dfd9bda984e61373d921cd60
Prathap <prathap.sims@gmail.com>
2023-02-12 01:07:20 -0800

    Add customers and orders files

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from '@/components/Header'
 import TopCard from '@/components/TopCard'
+import BarChart from '@/components/BarChart'
+import RecentOrders from '@/components/RecentOrders'
 
 
 
@@ -20,6 +22,10 @@ export default function Home() {
       <main className='bg-gray-100 min-h-screen'>
        <Header/>
        <TopCard/>
+       <div className='p-4 grid md:grid-cols-3 grid-cols-1 gap-4'>
+        <BarChart/>
+        <RecentOrders/>
+       </div>
       </main>
     </>
   )
